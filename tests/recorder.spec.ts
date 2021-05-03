8335b2cfc94edcee55224b6bca3ec90d29426e50
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-03 19:04:24 +0200

    feat: recorder

---
+++ tests/recorder.spec.ts
@@ -0,0 +1,29 @@
+(async () => {
+  await page.goto('https://www.ultebra.eu/');
+
+  await page.setViewport({ width: 2560, height: 1329 });
+
+  await page.waitForSelector('.col-sm-12 > #contact-form #nameContact');
+  await page.click('.col-sm-12 > #contact-form #nameContact');
+
+  await page.waitForSelector('.col-sm-12 > #contact-form #emailContact');
+  await page.click('.col-sm-12 > #contact-form #emailContact');
+
+  await page.waitForSelector('.col-sm-12 > #contact-form #message');
+  await page.click('.col-sm-12 > #contact-form #message');
+
+  await page.waitForSelector('#contact-form > .form-group > .input-group > .content-input > i');
+  await page.click('#contact-form > .form-group > .input-group > .content-input > i');
+
+  await page.waitForSelector('.row #sendContact');
+  await page.click('.row #sendContact');
+
+  await page.waitForSelector('.col-sm-12 > #contact-form #phoneContact');
+  await page.click('.col-sm-12 > #contact-form #phoneContact');
+
+  await page.waitForSelector('.row #sendContact');
+  await page.click('.row #sendContact');
+
+  await page.waitForSelector('.row #sendContact');
+  await page.click('.row #sendContact');
+})();
